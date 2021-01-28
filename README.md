1bcd5dcd4283d8ade4328403711e0bb59806e6e7
DimagaXIII <dimagalobodin@gmail.com>
2021-01-28 11:52:08 +0200

    re

--- README.md
+++
@@ -1,1 +0,0 @@
-# jonasliubov.github.io
---
+++ js/AddToCal.js
@@ -0,0 +1,26 @@
+$( document ).ready( function () {
+
+    // For all day startTime = date (20201010 - year,month,day), endTime(20201011 - year,month,day + 1)
+    document.getElementById('link-google').href = encodeURI(['https://www.google.com/calendar/render',
+        '?action=TEMPLATE',
+        '&text=' + ('Jonas And Liubov Wedding.'),
+        '&dates=' + ('20201010' || ''), // startTime
+        '/' + ('20201011' || ''), // endTime
+        '&details=' + ('WEDDING PARTY'),
+        '&location=' + ('Merelbekestraat 99, 9090 Melle, Belgium'),
+        '&sprop=&sprop=name:'].join(''));
+
+    var hrefA = encodeURI('data:text/calendar;charset=utf8,' + [
+        'BEGIN:VCALENDAR',
+        'VERSION:2.0',
+        'BEGIN:VEVENT',
+        'URL:' + document.URL,
+        'DTSTART:' + ('20201010' || ''),  // startTime
+        'DTEND:' + ('20201011' || ''), // endTime
+        'SUMMARY:' + ('Jonas And Liubov Wedding.'),
+        'DESCRIPTION:' + ('WEDDING PARTY'),
+        'LOCATION:' + ('Merelbekestraat 99, 9090 Melle, Belgium'),
+        'END:VEVENT',
+        'END:VCALENDAR'].join('\n'));
+    document.getElementById('link-apple').href = hrefA;
+});
